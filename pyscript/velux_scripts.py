9e7bf53c9b6ad68f616cf33757890d86b26ba306
root <root@homeassistant.local.hass.io>
2026-08-01 05:00:00 +0200

    Autocommit from HA - [2026.7.4]: 01-08-2026 05:00:00

--- pyscript/velux_scripts.py
+++ pyscript/velux_scripts.py
@@ -1,19 +1,57 @@
 import asyncio
 
+#@service
+#def close_entre_window():
+#    task.unique("close_entre_window")
+#    counter=0
+#    while binary_sensor.hue_secure_contact_sensor_entre_velux_opening == "on":
+#        log.info(f"Closing Entre velux window, attempt {counter}")
+#        button.press(entity_id="button.xiao_smart_ir_mate_c07738_close_window")
+#        asyncio.sleep(1)
+#        counter+=1
+#        
+#        # Never give up. Closing is important (like when it rains)
+#        if counter > 200:
+#            log.warning(    
+#                "Looks like the entre window isn't closing. "
+#                f"This is attempt {counter}. "
+#                f"Is the window sensor offline? (state={binary_sensor.hue_secure_contact_sensor_entre_velux_opening})"
+#            )
+            
 @service
 def close_entre_window():
     task.unique("close_entre_window")
     counter=0
     while binary_sensor.hue_secure_contact_sensor_entre_velux_opening == "on":
         log.info(f"Closing Entre velux window, attempt {counter}")
-        button.press(entity_id="button.xiao_smart_ir_mate_c07738_close_window")
-        asyncio.sleep(1)
+
+        remote.send_command(
+            entity_id="remote.broadlink_universal_remote",
+            num_repeats= 12,
+            delay_secs= 0.4,
+            hold_secs= 0,
+            command= "close_window",
+            device= "velux_window_remote"
+        )
+ 
+        asyncio.sleep(60)
         counter+=1
         
         # Never give up. Closing is important (like when it rains)
-        if counter > 200:
+        if counter > 10:
             log.warning(    
                 "Looks like the entre window isn't closing. "
                 f"This is attempt {counter}. "
                 f"Is the window sensor offline? (state={binary_sensor.hue_secure_contact_sensor_entre_velux_opening})"
             )
+            
+@service 
+def open_entre_window():
+    remote.send_command(
+        entity_id="remote.broadlink_universal_remote",
+        num_repeats= 12,
+        delay_secs= 0.4,
+        hold_secs= 0,
+        command= "open_window",
+        device= "velux_window_remote"
+    )
