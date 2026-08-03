125dffb0d47a4a1a1c44257e7497c13e7660f8b1
root <root@homeassistant.local.hass.io>
2026-08-03 05:00:00 +0200

    Autocommit from HA - [2026.7.4]: 03-08-2026 05:00:00

--- pyscript/velux_scripts.py
+++ pyscript/velux_scripts.py
@@ -1,23 +1,5 @@
 import asyncio
 
-#@service
-#def close_entre_window():
-#    task.unique("close_entre_window")
-#    counter=0
-#    while binary_sensor.hue_secure_contact_sensor_entre_velux_opening == "on":
-#        log.info(f"Closing Entre velux window, attempt {counter}")
-#        button.press(entity_id="button.xiao_smart_ir_mate_c07738_close_window")
-#        asyncio.sleep(1)
-#        counter+=1
-#        
-#        # Never give up. Closing is important (like when it rains)
-#        if counter > 200:
-#            log.warning(    
-#                "Looks like the entre window isn't closing. "
-#                f"This is attempt {counter}. "
-#                f"Is the window sensor offline? (state={binary_sensor.hue_secure_contact_sensor_entre_velux_opening})"
-#            )
-            
 @service
 def close_entre_window():
     task.unique("close_entre_window")
@@ -42,16 +24,31 @@ def close_entre_window():
             log.warning(    
                 "Looks like the entre window isn't closing. "
                 f"This is attempt {counter}. "
-                f"Is the window sensor offline? (state={binary_sensor.hue_secure_contact_sensor_entre_velux_opening})"
             )
-            
-@service 
+
+@service
 def open_entre_window():
-    remote.send_command(
-        entity_id="remote.broadlink_universal_remote",
-        num_repeats= 12,
-        delay_secs= 0.4,
-        hold_secs= 0,
-        command= "open_window",
-        device= "velux_window_remote"
-    )
+    task.unique("open_entre_window")
+    counter=0
+    while binary_sensor.hue_secure_contact_sensor_entre_velux_opening == "off":
+        log.info(f"Opening Entre velux window, attempt {counter}")
+
+        remote.send_command(
+            entity_id="remote.broadlink_universal_remote",
+            num_repeats= 12,
+            delay_secs= 0.4,
+            hold_secs= 0,
+            command= "open_window",
+            device= "velux_window_remote"
+        )
+ 
+        asyncio.sleep(60)
+        counter+=1
+        
+        if counter > 10:
+            log.warning(    
+                "Looks like the entre window isn't opening. "
+                f"This is attempt {counter}. "
+            )
+
+        
